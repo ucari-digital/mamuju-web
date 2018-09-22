32a132dbd1b8bcd9d5740f9a21e93481b9d28ac5
diatria <dimss.satria@gmail.com>
2018-09-22 20:38:39 +0700

    perbaikan link js

--- public/js/desktop.js
+++ public/js/desktop.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	$('.js-load').click(function(){
-		// window.location.href = $(this).data('link');
+		var link = $(this).data('link');
 		$('.loader').html('');
 		$('.loading').show();
 		$('.placeholder, .placeholder-text').hide();
-		$.get('http://localhost/mamuju/mamuju-web/public/viewer/Foto/julius-dein-tujukkan-trik-sulap-memukau-di-on-off-festival-2018-6', function(data) {
+		$.get(link, function(data) {
 			$('.loader').html(data);
 		}).done(function(){
 			$('.loading').hide();
