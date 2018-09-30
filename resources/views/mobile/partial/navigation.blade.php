f063b07fb5222fc6a3e4cad2db7c01a324bd329b
diatria <dimss.satria@gmail.com>
2018-09-30 15:51:56 +0700

    partial navigation

---
+++ resources/views/mobile/partial/navigation.blade.php
@@ -0,0 +1,42 @@
+{{-- ST MENU KATEGORI --}}
+<div class="block-nav">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-3 js-link nav-item" data-link="{{url('/kategori/infografis')}}">
+				<div class="item green active" id="infografis">
+					<div class="icon">
+						<i class="far fa-map"></i>
+					</div>
+					<span>Infografis</span>
+				</div>
+			</div>
+			<div class="col-md-3 col-3 js-link nav-item" data-link="{{url('/')}}">
+				<div class="item red active" id="populer">
+					<div class="icon">
+						<i class="far fa-fire">
+						<div class="m-label">Hot</div>
+						</i>
+					</div>
+					<span>Populer</span>
+				</div>
+			</div>
+			<div class="col-md-3 col-3 js-link nav-item" data-link="{{url('/kategori/Mamuju TV')}}">
+				<div class="item orange active" id="tv">
+					<div class="icon">
+						<i class="far fa-tv"></i>
+					</div>
+					<span>TV</span>
+				</div>
+			</div>
+			<div class="col-md-3 col-3 nav-item btn-more">
+				<div class="item orange" id="tv">
+					<div class="icon">
+						<i class="fas fa-th"></i>
+					</div>
+					<span>Lainnya</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+{{-- END MENU KETEGORI --}}
